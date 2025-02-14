de9655be508f3d380704931c3605cf6cb8bb36f1
hoangpc0112 <phamchinhhoang@gmail.com>
2025-02-14 11:29:33 +0700

    Update code

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,26 +29,26 @@ function Update-GlazeWM {
     }
 }
 
-function Update-Profile {
-    try {
-        $url = "https://raw.githubusercontent.com/hoangpc0112/PowerShell_Config/main/Microsoft.PowerShell_profile.ps1"
-        $oldhash = Get-FileHash $PROFILE
-        Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
-        $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
+# function Update-Profile {
+#     try {
+#         $url = "https://raw.githubusercontent.com/hoangpc0112/PowerShell_Config/main/Microsoft.PowerShell_profile.ps1"
+#         $oldhash = Get-FileHash $PROFILE
+#         Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
+#         $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
         
-        if ($newhash.Hash -ne $oldhash.Hash) {
-            Copy-Item -Path "$env:temp/Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
-            Write-Host "PowerShell profile updated. Please reload" -ForegroundColor Magenta
-        }
-        else {
-            Write-Host "PowerShell profile is up to date" -ForegroundColor Green
-        }
-    } catch {
-        Write-Error "PowerShell profile update failed"
-    } finally {
-        Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
-    }
-}
+#         if ($newhash.Hash -ne $oldhash.Hash) {
+#             Copy-Item -Path "$env:temp/Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
+#             Write-Host "PowerShell profile updated. Please reload" -ForegroundColor Magenta
+#         }
+#         else {
+#             Write-Host "PowerShell profile is up to date" -ForegroundColor Green
+#         }
+#     } catch {
+#         Write-Error "PowerShell profile update failed"
+#     } finally {
+#         Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
+#     }
+# }
 
 function Update-PowerShell {
     try {
@@ -69,11 +69,39 @@ function Update-PowerShell {
 
 if ($global:canConnectToGitHub) {
     Update-GlazeWM
-    Update-Profile
+    # Update-Profile
     Update-PowerShell
 }
 
-Write-Host "`n🚀 Hi HoangPC, what can I do for you today? ❤️`n" -ForegroundColor White
+$hostname = $env:COMPUTERNAME
+$username = $env:USERNAME
+$os = (Get-CimInstance Win32_OperatingSystem).Caption
+$cpu = (Get-CimInstance Win32_Processor).Name.Trim()
+$gpu = (Get-CimInstance Win32_VideoController | Select-Object -First 1).Name
+$ram_total = [math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
+$ram_free = [math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)
+$ram_used = $ram_total - $ram_free
+$shell = $PSVersionTable.PSVersion
+$resolution = (Get-CimInstance Win32_VideoController | Select-Object -First 1).CurrentHorizontalResolution.ToString() + "x" + (Get-CimInstance Win32_VideoController | Select-Object -First 1).CurrentVerticalResolution.ToString()
+
+$ascii_logo = @"
+
+[37m⡧⠡⠡⢑⣌⣍⣍⣍⣍⣍⣍⣍⣍⣦⠀⣷⡈⢢⠘⡄⠱⡈⡆⣨⣩⣩⣩⣩⣩⣩⣩⣩⣩⣡⡊⠌⠌⠌⢼   [0m[1;37m$hostname [0m@[1;37m$username[0m
+[37m⣿⣾⣨⣿⠿⠟⠛⠛⠉⠉⠙⠙⠛⠾⣧⣿⣿⡌⢂⠘⡀⠡⣱⣿⣿⠿⠛⠛⠋⠋⠙⠛⠛⠛⠿⢾⣅⣷⣿   [0m─────────────────────────────────────────
+[37m⠟⢋⣩⣤⣶⡶⣛⣭⣭⣭⣛⣿⣿⣷⣶⣿⣿⣿⡄⠣⢩⣾⣿⣿⣶⣾⣿⣿⣛⣭⣭⣭⣛⣿⣷⣦⣄⡙⠻   [0m[1;33mOS:        [0m[1;37m$os[0m
+[37m⣾⣿⣿⣿⡿⣾⣿⣿⣿⣿⣿⣷⢿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⡿⣾⣿⣿⣿⣿⣿⣷⢿⣿⣿⣿⣷   [0m[1;33mCPU:       [0m[1;37m$cpu[0m
+[37m⣿⣿⣿⣿⣇⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿   [0m[1;33mGPU:       [0m[1;37m$gpu[0m
+[37m⣿⣿⣿⣿⣿⣞⢿⣿⣿⣿⡿⣳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣞⢿⣿⣿⣿⡿⣳⣿⣿⣿⣿⣿   [0m[1;33mRes:       [0m[1;37m$resolution[0m
+[37m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿   [0m[1;33mMem:    [0m[1;37m$ram_used GB / $ram_total GB[0m
+[37m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣝⡻⡿⢿⢿⢿⢟⣮⡻⡿⡿⡿⢿⢟⣫⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿   [0m[1;33mShell:     [0m[1;37mPowerShell $shell[0m
+[37m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢹⣿⣿⣿⣿⣿⣿⣿⡏⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿   [0m
+[37m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⣿⡟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿   [0m[1;41m    [1;42m    [1;43m    [1;44m    [1;45m    [1;46m    [1;47m    [0m
+[37m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣭⣭⣭⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿   [0m[1;41m    [1;42m    [1;43m    [1;44m    [1;45m    [1;46m    [1;47m    [0m
+"@
+
+Write-Host $ascii_logo
+
+Write-Host "`n💡 Hi HoangPC, what can I do for you today? ❤️`n" -ForegroundColor White
 
 # Core utilities
 function which ($command) { Get-Command $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path }
